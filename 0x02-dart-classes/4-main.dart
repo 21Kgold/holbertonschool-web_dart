0462f9eb1ac3ffd234bbb99137680e62c24eb1f6
Claudia <claudiarpa@gmail.com>
2024-04-14 03:39:42 -0600

    4. Mutables

--- 0x02-dart-classes/4-main.dart
+++ 0x02-dart-classes/4-main.dart
@@ -1,9 +1,9 @@
 import '4-mutables.dart';
 void main() {
-  final password = Password(password: "Passwordecode");
-  print(password.isValid());
-  print(password.toString());
-  password.password = "Youssef4321";
-  print(password.isValid());
-  print(password.toString());
+  final passcode = Password(password: "Passwordecode");
+  print(passcode.isValid());
+  print(passcode.toString());
+  passcode.password = "Youssef4321";
+  print(passcode.isValid());
+  print(passcode.toString());
 }
