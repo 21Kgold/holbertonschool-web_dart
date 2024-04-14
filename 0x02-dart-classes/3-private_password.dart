b86bad4e03bba599ea64d8c7eb9d21f5d425ce72
Claudia <claudiarpa@gmail.com>
2024-04-14 02:32:30 -0600

    3. Private Password

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,24 @@
+class Password {
+    late String _password;
+
+    // Create a public setter
+    Password({required String password}) {
+        this._password = password;
+    }
+
+    bool isValid() {
+        if (_password.length >= 8 && _password.length <= 16) {
+            if (RegExp(r'[A-Z]').hasMatch(this._password) &&
+             RegExp(r'[a-z]').hasMatch(this._password) &&
+              RegExp(r'[0-9]').hasMatch(this._password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @override
+    String toString() {
+        return 'Your Password is: $_password';
+    }
+}
