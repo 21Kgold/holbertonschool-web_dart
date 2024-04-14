766d950ebe0ee54e97276f3aec3e5ad9c488b8f1
Claudia <claudiarpa@gmail.com>
2024-04-14 05:32:51 -0600

    5. Json to User

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -29,7 +29,7 @@ class User {
     }
 
     String toString() {
-        return 'User(id : ${id}, name: ${name}, age: ${age}, height: ${height})';
+        return 'User(id : ${id} ,name: ${name}, age: ${age}, height: ${height})';
     }
 
     String showName() {
